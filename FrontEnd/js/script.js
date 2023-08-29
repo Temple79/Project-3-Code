f8c18b55c080e1f69e1d0b9452ab2dbeab20d7b8
Petar <topchiyski@hotmail.fr>
2023-08-29 17:18:15 +0300

    Created Filter Button Initial

--- FrontEnd/js/script.js
+++ FrontEnd/js/script.js
@@ -1,5 +1,6 @@
 const api_url="http://localhost:5678/api/works";
 const gallery=document.querySelector(".gallery");
+const portfolio=document.querySelector("#portfolio")
 
 async function getApiData(url){
     const response=await fetch(url);
@@ -26,9 +27,22 @@ function currentGallery(data){
     }
 } 
 
+function createFilterButtons(){
+    const buttonsArray=["Tous","Objets","Appartements","Hôtels & Restaurants"]
+    const divFilter=document.createElement("div")
+    buttonsArray.forEach(element => {
+        let button=document.createElement("button")
+        button.textContent=element
+        divFilter.appendChild(button)
+    });
+    portfolio.appendChild(divFilter)
+}
 
+// Initial Page Load
 async function initialPageLoad(){
     const data=await getApiData(api_url)
+    createFilterButtons()
     currentGallery(data)
 }
 initialPageLoad()
+
