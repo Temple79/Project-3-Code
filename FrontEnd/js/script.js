38bf418a06a84121163960da983b3c5d8fcc9461
Petar <topchiyski@hotmail.fr>
2023-08-30 12:28:29 +0300

    Filter Done

--- FrontEnd/js/script.js
+++ FrontEnd/js/script.js
@@ -2,6 +2,7 @@ const api_url="http://localhost:5678/api/works";
 const gallery=document.querySelector(".gallery");
 const portfolio=document.querySelector("#portfolio")
 
+
 async function getApiData(url){
     const response=await fetch(url);
     const data=await response.json();
@@ -33,9 +34,51 @@ function createFilterButtons(){
     buttonsArray.forEach(element => {
         let button=document.createElement("button")
         button.textContent=element
+        // button.classList.add("btn")
+        // button.style.color="#1D6154"
+        // button.style.borderRadius="60px"
+        // button.style.borderColor="#1D6154"
+        // button.style.height="37px"
+        // button.style.fontStyle="bold"
         divFilter.appendChild(button)
     });
-    portfolio.appendChild(divFilter)
+    divFilter.addEventListener("click", eventHandler)
+
+    gallery.before(divFilter)
+
+}
+
+async function eventHandler (event) {
+    if(event.target.tagName === "BUTTON") {
+        const data = await getApiData(api_url)
+        const currentButton=event.target
+        if(currentButton.textContent === "Tous") {
+            currentGallery(data)
+            return
+        }
+        if(currentButton.textContent === "Objets") {
+            const objetsData = data.filter((x)=> {
+                return x.categoryId === 1;
+            })
+            currentGallery(objetsData)
+            return
+        }
+        if(currentButton.textContent === "Appartements") {
+            const appartementsData = data.filter((y)=> {
+                return y.categoryId === 2;
+            })
+            currentGallery(appartementsData)
+            return
+        }            
+        if(currentButton.textContent === "Hôtels & Restaurants") {
+            const hotelsData = data.filter((z)=> {
+                return z.categoryId === 3;
+            })
+            currentGallery(hotelsData)
+            return
+        }
+
+    }
 }
 
 // Initial Page Load
@@ -44,5 +87,9 @@ async function initialPageLoad(){
     createFilterButtons()
     currentGallery(data)
 }
+
+
 initialPageLoad()
+
+
 
