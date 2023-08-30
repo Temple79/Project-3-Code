d0dd8a08e34607151b6b0e59833e4392fae15f61
Petar <topchiyski@hotmail.fr>
2023-08-30 16:33:33 +0300

    User login test

--- FrontEnd/js/login.js
+++ FrontEnd/js/login.js
@@ -1,15 +1,30 @@
 const formLogin = document.querySelector("#formLogin")
 const incorrect = document.querySelector(".incorrect")
+const urlLogin = 'http://localhost:5678/api/users/login'
 
-formLogin.addEventListener("submit", login)
+formLogin.addEventListener("submit", ajoutForm)
 
-async function login (event) {
+async function ajoutForm (event) {
     event.preventDefault()
 
     const formData = new FormData(formLogin)
     const email = formData.get("email") 
     const password = formData.get("password")
 
-    console.log(email)
-    console.log(password)
+    console.log(email,password)
+
+    login(email, password)
+}
+
+async function login (email, password) {
+    const user = {email: 'email', password: 'password'}
+    let response = await fetch('http://localhost:5678/api/users/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(user)
+    })
+    let result = await response.json()
+    console.log(result)
 }
