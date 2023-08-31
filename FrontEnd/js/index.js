eeff118b443e01d211bc163cbe9738d0ed494b64
Petar <topchiyski@hotmail.fr>
2023-08-31 18:37:11 +0300

    Page Mode Edition Start

--- FrontEnd/js/index.js
+++ FrontEnd/js/index.js
@@ -22,13 +22,35 @@ function checkIsAdmin()
 }
 
 function iAmAnAdmin() {
-    alert ("je suis un admin");
+    const buttonLogin = document.querySelector("#buttonLogin")
+    buttonLogin.textContent = "Logout"
+    createBannerEdition()
 }
 function iAmNotAnAdmin() {
-    alert ("je ne suis pas un admin");
+    alert ("je ne suis pas un admin")
 }
 
 initialPageLoad()
 
+function createBannerEdition() {
+    const header = document.querySelector("header")
+    const bannerEdtion = document.createElement("div")
+    const modeEdition = document.createElement("span")
+    const editImage = document.createElement("img")
 
+    editImage.src = "./assets/icons/Edit_icon.png"
+    editImage.alt = "Edit icon"
+
+    modeEdition.appendChild(editImage)
+
+    modeEdition.textContent = "Mode édition"
+
+    bannerEdtion.style.backgroundColor = "red"
+    bannerEdtion.style.height = "50px"
+    modeEdition.style.color = "white"
+    modeEdition.style.fontSize = "10px"
+
+    bannerEdtion.appendChild(modeEdition)
+    header.before(bannerEdtion)
+}
 
